62dc69b70c41d92860652232553c3c6256e718f6
Blackc <15601222522@163.com>
2021-07-08 10:49:22 +0800

    test

---
+++ src/main/java/blackc/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package blackc.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Blackc
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
